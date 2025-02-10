84099b08d39e7cc6b7541ad213922a5b88148dfa
Tsunghao <tsunghao.chen12@gmail.com>
2025-02-10 22:26:54 +0800

    fixed norminette issue

--- python_0_Starting/ex09/ft_package/src/ft_package/__init__.py
+++ python_0_Starting/ex09/ft_package/src/ft_package/__init__.py
@@ -1,17 +1,26 @@
-from .count_in_list import count_in_list
-
 # __init__.py serves as the package's entry point
 # Here you define what gets "EXPOSED" when someone else
 # import this pacakge.
 
+# from .count_in_list import count_in_list
 # from .count_in_list -> means look for the "count_in_list" module
 # in this package
 # you can have several modules in a package
 
-from .calculation import num_plus_one
+# from .calculation import num_plus_one
 
 # you can do "from ft_package import num_plus_one" directly
 # however, we need to explicitly say "calculation.num_minus_one()"
 # function since it is not exposed here
 
 # Or, you can define a function here directly
+
+
+def count_in_list(list, word):
+    count = 0
+    if not list or not word:
+        return count
+    for item in list:
+        if item == word:
+            count += 1
+    return count
