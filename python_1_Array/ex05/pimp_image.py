4d5e381ca2ce1135880b4aa736a05b6c0c3e3b60
Tsunghao <tsunghao.chen12@gmail.com>
2025-02-19 13:33:27 +0100

    fix norm

--- python_1_Array/ex05/pimp_image.py
+++ python_1_Array/ex05/pimp_image.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 
+
 def ft_invert(array: np.ndarray) -> np.ndarray:
     '''Invert the color of the image received.'''
     # Check input
@@ -10,7 +11,7 @@ def ft_invert(array: np.ndarray) -> np.ndarray:
         raise ValueError("Error: Empty input")
     if array.ndim not in [2, 3]:
         raise ValueError("Error: Wrong array size, must be 2D or 3D array")
-    
+
     # Process image
     inverted = 255 - array
     print(inverted)
@@ -19,16 +20,17 @@ def ft_invert(array: np.ndarray) -> np.ndarray:
     Image.fromarray(inverted).show()
     return inverted
 
+
 def ft_red(array: np.ndarray) -> np.ndarray:
     '''Filter red color of the image received.'''
     # Check input
     if not isinstance(array, np.ndarray):
         raise ValueError("Error: wrong input format")
     if array is None:
-        raise ValueError("Error: Empty input") 
+        raise ValueError("Error: Empty input")
     if array.ndim != 3:
         raise ValueError("Error: Wrong array size, must be 3D array")
-    
+
     # Process image
     red = np.zeros_like(array)
     red[:, :, 0] = array[:, :, 0]
@@ -38,16 +40,17 @@ def ft_red(array: np.ndarray) -> np.ndarray:
     Image.fromarray(red).show()
     return red
 
+
 def ft_green(array: np.ndarray) -> np.ndarray:
     '''Filter green color of the image received.'''
     # Check input
     if not isinstance(array, np.ndarray):
         raise ValueError("Error: wrong input format")
     if array is None:
-        raise ValueError("Error: Empty input") 
+        raise ValueError("Error: Empty input")
     if array.ndim != 3:
         raise ValueError("Error: Wrong array size, must be 3D array")
-    
+
     # Process image
     green = np.zeros_like(array)
     green[:, :, 1] = array[:, :, 1]
@@ -57,16 +60,17 @@ def ft_green(array: np.ndarray) -> np.ndarray:
     Image.fromarray(green).show()
     return green
 
+
 def ft_blue(array: np.ndarray) -> np.ndarray:
     '''Filter blue color of the image received.'''
     # Check input
     if not isinstance(array, np.ndarray):
         raise ValueError("Error: wrong input format")
     if array is None:
-        raise ValueError("Error: Empty input") 
+        raise ValueError("Error: Empty input")
     if array.ndim != 3:
         raise ValueError("Error: Wrong array size, must be 3D array")
-    
+
     # Process image
     # Use zeros_like to create a zero matrix with same shape
     blue = np.zeros_like(array)
@@ -77,21 +81,22 @@ def ft_blue(array: np.ndarray) -> np.ndarray:
     Image.fromarray(blue).show()
     return blue
 
+
 def ft_grey(array: np.ndarray) -> np.ndarray:
     '''Change to grayscaled image'''
     # Check input
     if not isinstance(array, np.ndarray):
         raise ValueError("Error: wrong input format")
     if array is None:
-        raise ValueError("Error: Empty input") 
+        raise ValueError("Error: Empty input")
     if array.ndim != 3:
         raise ValueError("Error: Wrong array size, must be 3D array")
-    
+
     # Process image
     red_channel = array[:, :, 0] / 3
     green_channel = array[:, :, 1] / 3
     blue_channel = array[:, :, 2] / 3
-    grey_channel = red_channel + green_channel +blue_channel
+    grey_channel = red_channel + green_channel + blue_channel
     grey = np.stack([grey_channel, grey_channel, grey_channel], axis=2)
 
     # Display image
