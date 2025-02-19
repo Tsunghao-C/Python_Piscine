0af8c263d3cd54ca6acfacfaa82d4ba5d116b9b0
Tsunghao <tsunghao.chen12@gmail.com>
2025-02-19 15:40:35 +0100

    optimize with notes

--- python_0_Starting/ex02/find_ft_type.py
+++ python_0_Starting/ex02/find_ft_type.py
@@ -1,30 +1,21 @@
 def all_thing_is_obj(obj: any) -> int:
     obj_types = {
-        "List" : "list",
-        "Tuple" : "tuple",
-        "Set" : "set",
-        "Dict" : "dict",
-        "is in the kitchen" : "str"
+        list: "List",
+        tuple: "Tuple",
+        set: "Set",
+        dict: "Dict",
+        str: "String"
     }
-    for name, data_type in obj_types.items():
-        if (type(obj).__name__ == data_type):
-            if data_type == "str":
-                print(f"{obj} {name} : {type(obj)}")
-            else:
-                print(f"{name} : {type(obj)}")
-            return 42
-    print("Type not found")
+
+    type_name = obj_types.get(type(obj), "Type not found")
+    if type(obj) is str:
+        print(f"{obj} is in the kitchen : {type(obj)}")
+    elif type_name in obj_types.values():
+        print(f"{type_name} : {type(obj)}")
+    else:
+        print(type_name)
     return 42
 
-# ft_list = ["Hello", "tata!"]
-# ft_tuple = ("Hello", "toto!")
-# ft_set = {"Hello", "tutu!"}
-# ft_dict = {"Hello" : "titi!"}
-
-# all_thing_is_obj(ft_list)
-# all_thing_is_obj(ft_tuple)
-# all_thing_is_obj(ft_set)
-# all_thing_is_obj(ft_dict)
-# all_thing_is_obj("Brian")
-# all_thing_is_obj("Toto")
-# print(all_thing_is_obj(10))
+# type(obj) returns the type of the object
+# use "is" to compare datatype, mem address
+# use "==" operator to comparet the referenced value
