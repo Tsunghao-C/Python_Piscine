ef253204cb05bfeb83bcb24650c939bb299dc048
Tsunghao <tsunghao.chen12@gmail.com>
2025-02-19 15:00:51 +0100

    Added comma

--- python_0_Starting/ex01/format_ft_time.py
+++ python_0_Starting/ex01/format_ft_time.py
@@ -1,7 +1,14 @@
 import time
+import locale
+
+
+# Set locale to use comma as a thousand separator
+locale.setlocale(locale.LC_ALL, '')
 
 current_time = time.time()
+formatted_time = locale.format_string("%.4f", current_time, grouping=True)
 scientific_not_time = "{:.2e}".format(current_time)
 
-print(f"Seconds since January 1, 1970: {current_time} or {scientific_not_time} in scientific notation")
+print(f"Seconds since January 1, 1970: \
+{formatted_time} or {current_time:.2e} in scientific notation")
 print(time.strftime("%b %d %Y"))
