88825de6dfea079737e2d74d7cebc51b4740c8f5
Tsunghao <tsunghao.chen12@gmail.com>
2025-02-19 15:47:47 +0100

    fix norm

--- python_0_Starting/ex03/NULL_not_found.py
+++ python_0_Starting/ex03/NULL_not_found.py
@@ -1,13 +1,9 @@
-# `is` used to compare if mem address, us 'is' for None (NULL) comparison
-# `==` used to compare if value is identical
-# Nan is a special case that "nan is nan" is alwasy false. Use str(number) == 'nan' or math.isnan() instead!
-
 def NULL_not_found(object: any) -> int:
-    null_values = {"Nothing" : None,
-                   "Cheese" : float("NaN"),
-                   "Zero" : 0,
-                   "Empty" : '',
-                   "Fake" : False}
+    null_values = {"Nothing": None,
+                   "Cheese": float("NaN"),
+                   "Zero": 0,
+                   "Empty": '',
+                   "Fake": False}
     if isinstance(object, float) and str(object) == 'nan':
         print(f"Cheese : nan {type(object)}")
         return 0
@@ -18,14 +14,7 @@ def NULL_not_found(object: any) -> int:
     print("Type not Found")
     return 1
 
-# Nothing = None
-# Garlic = float("NaN")
-# Zero = 0
-# Empty = ''
-# Fake = False
-# NULL_not_found(Nothing)
-# NULL_not_found(Garlic)
-# NULL_not_found(Zero)
-# NULL_not_found(Empty)
-# NULL_not_found(Fake)
-# print(NULL_not_found("Brian"))
+# `is` used to compare if mem address, us 'is' for None (NULL) comparison
+# `==` used to compare if value is identical
+# Nan is a special case that "nan is nan" is alwasy false. \
+# Use str(number) == 'nan' or math.isnan() instead!
