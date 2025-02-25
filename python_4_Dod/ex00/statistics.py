0ed60f43700734a221a7672fa1ed4a9e824caa07
Tsunghao Chen <tsuchen@paul-f5Ar1s1.clusters.42paris.fr>
2025-02-25 10:20:38 +0100

    Added more decorators to simplify error handling

--- python_4_Dod/ex00/statistics.py
+++ python_4_Dod/ex00/statistics.py
@@ -1,27 +1,35 @@
 class Statistics():
     """Class that do no instatiate and do statistic calculations"""
+
     @staticmethod
+    def valid_input(func):
+        """Decorator to validate input list"""
+        def wrapper(arr):
+            if not arr:
+                raise ValueError("Error: input list cannot be empty")
+            return (func(arr))
+        return wrapper
+
+    @staticmethod
+    @valid_input
+    # the order of staticmethod and valid_input matters!!!
     def mean(arr: list[int | float]) -> float:
         """returns mean of the arr"""
-        if not arr:
-            raise ValueError("Empty list")
         return sum(arr) / len(arr)
 
     @staticmethod
-    def median(arr: list[int | float]) -> float | int:
+    @valid_input
+    def median(arr: list[int | float]) -> float:
         """returns median value of the sorted arr"""
-        if not arr:
-            raise ValueError("Empty list")
         arr = sorted(arr)
         n = len(arr)
         mid = n // 2
         return (arr[mid] if n % 2 != 0 else (arr[mid - 1] + arr[mid]) / 2)
 
     @staticmethod
-    def quartile(arr: list[int | float]) -> list[float, float]:
+    @valid_input
+    def quartile(arr: list[int | float]) -> list[float]:
         """returns the range from 1/4 to 3/4 of the arr"""
-        if not arr:
-            raise ValueError("Empty list")
         arr = sorted(arr)
         n = len(arr)
         quar_1 = float(arr[n // 4])
@@ -29,19 +37,17 @@ class Statistics():
         return [quar_1, quar_3]
 
     @staticmethod
+    @valid_input
     def std(arr: list[int | float]) -> float:
         """returns standard deviation of the arr"""
-        if not arr:
-            raise ValueError("Empty list")
         mean = sum(arr) / len(arr)
         var = sum((x - mean) ** 2 for x in arr) / len(arr)
         return var ** 0.5
 
     @staticmethod
+    @valid_input
     def var(arr: list[int | float]) -> float:
         """returns the variance of the arr"""
-        if not arr:
-            raise ValueError("Empty list")
         mean = sum(arr) / len(arr)
         return sum((x - mean) ** 2 for x in arr) / len(arr)
 
